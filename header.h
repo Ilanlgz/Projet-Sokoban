2921869abc3abe4b237a9840c682ff77f56ced5d
Korityx <ilan.langelez@coda-student.school>
2024-10-13 16:40:35 +0200

    Create header.h

---
+++ header.h
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#ifndef HEADER_H
+#define HEADER_H
+
+typedef struct {
+    int x;
+    int y;
+    char movement;
+
+} Joueur;
+
+typedef struct {
+    int x;
+    int y;
+    
+} Boite;
+
+typedef struct {
+    int x; 
+    int y;
+} Emplacement;
+
+#endif
+
+
