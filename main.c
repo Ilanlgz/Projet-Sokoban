e2ee68f09c038ecf5ecadaeccf557f2b5f1116f0
Korityx <ilan.langelez@coda-student.school>
2024-10-13 16:39:40 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,41 +1,41 @@
 #include "header.h"
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+// Initialisation de la planche
+char **init_board() {
 
-char **init_board()
-{
     char **board;
     int i = 0;
     int j;
 
     board = malloc(10 * sizeof(*board));
-  
-    while(i < 10)
-    {
+    if (board == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    while (i < 10) {
         board[i] = malloc(10 * sizeof(char));
+        if (board[i] == NULL) {
+            exit(EXIT_FAILURE);
+        }
         j = 0;
-        while(j < 8)
-        {
+        while (j < 8) { // Changer j < 8 à j < 10
             board[i][j] = ' ';
             j++;
         }
         i++;
-      
-
     }
     return board;
 }
-void display_board(char **board)
-{
+
+// Affichage de la planche
+void display_board(char **board) {
     int i = 0;
-    int j = 0; 
-     
-    
 
     printf("\n");
-
-
-while(i < 10)
+    while(i < 10)
 {
     if( i < 1)
     {
@@ -54,179 +54,228 @@ while(i < 10)
 }
 }
 
+// Placement d'emplacement
 void startEmplacement(char **board, Emplacement *empPos) {
     int x, y;
     do {
-        x = rand() % 6;
-        y = rand() % 6;
-    }while(board[x][y] != ' ');
+        x = rand() % 6 + 2;
+        y = rand() % 6 + 2;
+    } while (board[x][y] != ' ');
 
     board[x][y] = '.';
     empPos->x = x;
     empPos->y = y;
-       
 }
+
+// Placement de boîte
 void startBoite(char **board, Boite *boitePos) {
     int x, y;
-    do{
-      x = rand() % 6 + 2;
-      y = rand() % 6 + 2;
+    do {
+        x = rand() % 6 + 2;
+        y = rand() % 6 + 2;
+    } while (board[x][y] != ' ');
 
-    }while(board[x][y]  != ' ');
-
-     board[x][y] = 'X';
+    board[x][y] = 'X';
     boitePos->x = x;
     boitePos->y = y;
 }
+
+// Placement de joueur
 void startJoueur(char **board, Joueur *playerPos) {
     int x, y;
-    do{
+    do {
         x = rand() % 6 + 2;
         y = rand() % 6 + 2;
-    }while(board[x][y] != ' ');
+    } while (board[x][y] != ' ');
 
     board[x][y] = 'o';
     playerPos->x = x;
     playerPos->y = y;
-    
 }
 
-void start(char **board, Joueur *playerPos, Boite *boitePos,  Emplacement *empPos){
+// Démarrage du jeu
+void start(char **board, Joueur *playerPos, Boite *boitePos, Emplacement *empPos) {
     startJoueur(board, playerPos);
     startBoite(board, boitePos);
     startEmplacement(board, empPos);
 }
 
-void movement(char **board, Joueur *playerPos, Emplacement *empPos) {
+void movement(char **board, Joueur *playerPos, Boite *boitePos, Emplacement *empPos) {
     printf("Choisissez un mouvement : haut(z) bas(s) gauche(q) droite(d)\n");
-    scanf("%c",&playerPos->movement);
+    scanf(" %c", &playerPos->movement); // Espace avant %c pour gérer le retour à la ligne
     int move_x = playerPos->x;
     int move_y = playerPos->y;
 
-    if(playerPos->movement == 'z'){
+    // Déterminer le mouvement du joueur
+    if (playerPos->movement == 'z') {
         move_x--;
-    }else if(playerPos->movement == 's') {
+    } else if (playerPos->movement == 's') {
         move_x++;
-    }else if(playerPos->movement == 'q'){
+    } else if (playerPos->movement == 'q') {
         move_y--;
-    }else if(playerPos->movement == 'd') {
+    } else if (playerPos->movement == 'd') {
         move_y++;
-    }else {
-        printf("Erreur de frappe veuillez ressaisir une touche correct\n");
+    } else {
+        printf("Erreur de frappe, veuillez ressaisir une touche correcte\n");
         return;
     }
 
+    // Vérifier si le joueur est adjacent à la boîte
+    if (move_x == boitePos->x && move_y == boitePos->y) {
+        // Déplacer la boîte dans la même direction que le joueur
+        if (playerPos->movement == 'z') { // Déplacement vers le haut
+            if (board[boitePos->x - 1][boitePos->y] == ' ') {
+                board[boitePos->x][boitePos->y] = ' '; // Libérer l'ancienne position de la boîte
+                boitePos->x--; // Déplacer la boîte vers le haut
+                board[boitePos->x][boitePos->y] = 'X'; // Placer la boîte à la nouvelle position
+                playerPos->x = move_x; // Déplacer le joueur
+                board[playerPos->x][playerPos->y] == ' ';
+            }
+        } else if (playerPos->movement == 's') { // Déplacement vers le bas
+            if (board[boitePos->x + 1][boitePos->y] == ' ') {
+                board[boitePos->x][boitePos->y] = ' '; // Libérer l'ancienne position de la boîte
+                boitePos->x++; // Déplacer la boîte vers le bas
+                board[boitePos->x][boitePos->y] = 'X'; // Placer la boîte à la nouvelle position
+                playerPos->x = move_x; // Déplacer le joueur
+                board[playerPos->x][playerPos->y] == ' ';
+            }
+        } else if (playerPos->movement == 'q') { // Déplacement vers la gauche
+            if (board[boitePos->x][boitePos->y - 1] == ' ') {
+                board[boitePos->x][boitePos->y] = ' '; // Libérer l'ancienne position de la boîte
+                boitePos->y--; // Déplacer la boîte à gauche
+                board[boitePos->x][boitePos->y] = 'X'; // Placer la boîte à la nouvelle position
+                playerPos->y = move_y; // Déplacer le joueur
+                board[playerPos->x][playerPos->y] == ' ';
+            }
+        } else if (playerPos->movement == 'd') { // Déplacement vers la droite
+            if (board[boitePos->x][boitePos->y + 1] == ' ') {
+                board[boitePos->x][boitePos->y] = ' '; // Libérer l'ancienne position de la boîte
+                boitePos->y++; // Déplacer la boîte à droite
+                board[boitePos->x][boitePos->y] = 'X'; // Placer la boîte à la nouvelle position
+                playerPos->y = move_y; // Déplacer le joueur
+                board[playerPos->x][playerPos->y] == ' ';
+            }
+        }
+    } else {
+        // Si le joueur ne déplace pas la boîte, mettez à jour sa position
+        if (board[move_x][move_y] == ' ' || board[move_x][move_y] == 'o') {
+            board[playerPos->x][playerPos->y] = ' '; // Libérer l'ancienne position du joueur
+            board[move_x][move_y] = 'o'; // Placer le joueur à la nouvelle position
+            playerPos->x = move_x; // Mettre à jour la position du joueur
+            playerPos->y = move_y; // Mettre à jour la position du joueur
+            board[playerPos->x][playerPos->y] == ' ';
+        }
+    }
 
-    if(board[move_x][move_y] = ' ' || board[move_x][move_y] == 'o') {
-
-        board[playerPos->x][playerPos->y] == ' ';
-        board[move_x][move_y] == 'o';
-        playerPos->x = move_x;
-        playerPos->y = move_y; 
-    } 
-
-    if(board[empPos->x][empPos->y] == ' '){
-        board[empPos->x][empPos->y] == '.';
-
+    // Vérification si la boîte est sur l'emplacement
+    if (boitePos->x - 1 == empPos->x && boitePos->y - 1 == empPos->y && boitePos->x + 1 == empPos->x && boitePos->y + 1 == empPos->y) {
+        printf("Bien joué c'était un peu facile nan ?\n");
+        exit(0);
     }
 }
-    void moveboite(char **board, Boite *boitePos, Joueur *playerPos, Emplacement *empPos) {
-        int move_x = boitePos->x;
-        int move_y = boitePos->y;
 
-        if(playerPos->x == boitePos->x && playerPos->y == boitePos->y && playerPos->movement == 's'){
-           playerPos->x = move_x++;
-        }
-        if(board[move_x][move_y] == ' ' || '.'){
-            board[boitePos->x][boitePos->y] = ' ';
-            board[move_x][move_y] = 'X';
-            boitePos->x = move_x;
-            board[playerPos->x][playerPos->y] = ' ';
-            playerPos->x++;
-            board[playerPos->x][playerPos->y] = 'o';
-        }else {
-            printf("Vous ne pouvez pas déplacer la boîte vers le bas\n");
-        }
-    
-        if(playerPos->x == boitePos->x && playerPos->y == boitePos->y && playerPos->movement == 'z'){
-           playerPos->x = move_x--;
-        }
-        if(board[move_x][move_y] == ' ' || '.'){
+
+// Mouvement de la boîte
+void moveboite(char **board, Boite *boitePos, Joueur *playerPos, Emplacement *empPos) {
+    int move_x = boitePos->x;
+    int move_y = boitePos->y;
+
+    if (playerPos->x == boitePos->x && playerPos->y == boitePos->y && playerPos->movement == 's') {
+        move_x++; // Déplacer la boîte vers le bas
+    }
+
+    // Condition corrigée
+    if (board[move_x][move_y] == ' ') {
+        board[boitePos->x][boitePos->y] = ' '; // Libérer l'ancienne position
+        board[move_x][move_y] = 'X'; // Déplacer la boîte
+        boitePos->x = move_x; // Mettre à jour la position de la boîte
+        boitePos->y = move_y; // Mettre à jour la position de la boîte
+    }
+
+
+    // Déplacement vers le haut
+    if (playerPos->x == boitePos->x && playerPos->y == boitePos->y && playerPos->movement == 'z') {
+        move_x--;
+        if (board[move_x][move_y] == ' ') {
             board[boitePos->x][boitePos->y] = ' ';
             board[move_x][move_y] = 'X';
             boitePos->x = move_x;
             board[playerPos->x][playerPos->y] = ' ';
             playerPos->x--;
             board[playerPos->x][playerPos->y] = 'o';
-        }else {
+        } else {
             printf("Vous ne pouvez pas déplacer la boîte vers le haut\n");
         }
-    
-    
-        if(playerPos->x == boitePos->x && playerPos->y == boitePos->y - 1 && playerPos->movement == 'd'){
-           playerPos->y = move_y++;
-        }
-        if(board[move_x][move_y] == ' ' || '.'){
+    }
+
+    // Déplacement vers la droite
+    if (playerPos->x == boitePos->x && playerPos->y == boitePos->y && playerPos->movement == 'd') {
+        move_y++;
+        if (board[move_x][move_y] == ' ') {
             board[boitePos->x][boitePos->y] = ' ';
             board[move_x][move_y] = 'X';
             boitePos->y = move_y;
             board[playerPos->x][playerPos->y] = ' ';
             playerPos->y++;
             board[playerPos->x][playerPos->y] = 'o';
-        }else {
+        } else {
             printf("Vous ne pouvez pas déplacer la boîte vers la droite\n");
         }
-        
-    
-        if(playerPos->x == boitePos->x && playerPos->y == boitePos->y && playerPos->movement == 'q'){
-            playerPos->y = move_y--;
-                if(board[move_x][move_y] == ' ' || '.') {
-                    board[boitePos->x][boitePos->y] = ' ';
-                    board[move_x][move_y] = 'X';
-                    boitePos->y = move_y;
-                    board[playerPos->x][playerPos->y] = ' ';
-                    playerPos->y--;
-                    board[playerPos->x][playerPos->y] = 'o';     
-                }else{
-                printf("Vous ne pouvez pas aller plus à gauche\n");
-        }
-        
     }
-    
-    if(boitePos->x = empPos->x && boitePos->y == empPos->y){
+
+    // Déplacement vers la gauche
+    if (playerPos->x == boitePos->x && playerPos->y == boitePos->y && playerPos->movement == 'q') {
+        move_y--;
+        if (board[move_x][move_y] == ' ') {
+            board[boitePos->x][boitePos->y] = ' ';
+            board[move_x][move_y] = 'X';
+            boitePos->y = move_y;
+            board[playerPos->x][playerPos->y] = ' ';
+            playerPos->y--;
+            board[playerPos->x][playerPos->y] = 'o';
+        } else {
+            printf("Vous ne pouvez pas aller plus à gauche\n");
+        }
+    }
+
+    // Vérification si la boîte est sur l'emplacement
+    if (boitePos->x == empPos->x && boitePos->y == empPos->y) {
         printf("Bien joué c'était un peu facile nan ?\n");
         exit(0);
     }
-    if((boitePos->x == 1 && boitePos->y == 1) ||
-      (boitePos->x == 1 && boitePos->y == 8) || 
-      (boitePos->x == 8 && boitePos->y == 1) || 
-      (boitePos->x == 8 && boitePos->y == 8)) {
+
+    // Vérification si la boîte est coincée
+    if ((boitePos->x == 1 && boitePos->y == 1) ||
+        (boitePos->x == 1 && boitePos->y == 8) || 
+        (boitePos->x == 8 && boitePos->y == 1) || 
+        (boitePos->x == 8 && boitePos->y == 8)) {
         printf("Vous avez perdu ! La boîte est coincée.\n");
         exit(0); 
     }
-    }
+}
 
-    int main(){
 
-        char **board;
-        char mouvement;
-        Joueur playerPos;
-        Boite boitePos;
-        Emplacement empPos;
-        srand(time(NULL));
-        board = init_board(); 
-        start(board,&playerPos, &boitePos, &empPos);
-        display_board(board);
-    
+int main() {
+    char **board;
+    Joueur playerPos;
+    Boite boitePos;
+    Emplacement empPos;
+    srand(time(NULL));
+    board = init_board(); 
+    start(board, &playerPos, &boitePos, &empPos);
+    display_board(board);
 
-    while(1) {
+    while (1) {
         system("clear");
-        printf("Joueur : %d,%d\nEmplacement : %d,%d\nBoite : %d,%d\n", playerPos.x,playerPos.y,empPos.x,empPos.y,boitePos.x,boitePos.y);
+        printf("Joueur : %d,%d\nEmplacement : %d,%d\nBoite : %d,%d\n", playerPos.x, playerPos.y, empPos.x, empPos.y, boitePos.x, boitePos.y);
         display_board(board);
-        movement(board, &playerPos, &empPos);
-        moveboite(board, &boitePos, &playerPos, &empPos);
-
-    }   return 0;
+        movement(board, &playerPos, &boitePos, &empPos);
+        moveboite(board, &boitePos, &playerPos, &empPos); // Appel de moveboite avec les bons paramètres
     }
 
+    
+    free(board);
+
+    return 0;
+}
 
